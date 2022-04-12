f3b2d18dfc2d1d7b3a1a89201107a4909091ab12
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2022-04-12 15:06:54 +0300

    12042022_1

---
+++ android/app/src/main/kotlin/com/example/bosproje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bosproje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Uygulamam());
+}
+
+class Uygulamam extends StatelessWidget {
+  const Uygulamam({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
